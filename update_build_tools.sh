13eb33923f09c8654f4a87f83a97a27d8280b4ab
spencewenski <sferris37@gmail.com>
2015-08-12 13:05:41 -0400

    fix update build tools script?

--- update_build_tools.sh
+++ update_build_tools.sh
@@ -2,11 +2,10 @@
 
 # update g++
 # g++4.8.1
-if [ "$CXX" == "g++" ]
-  then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  sudo apt-get update -qq
-  sudo apt-get install -qq g++-4.8
-  export CXX="g++-4.8"
+if [ "$CXX" = "g++" ]
+  then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+    sudo apt-get install -qq g++-4.8
+    export CXX="g++-4.8"
 fi
-
-echo $CXX
