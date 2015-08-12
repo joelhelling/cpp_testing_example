19b7790ef85a7df589f14cb03206f0164b2cf288
spencewenski <sferris37@gmail.com>
2015-08-12 13:30:09 -0400

    fixing travis

--- update_build_tools.sh
+++ update_build_tools.sh
@@ -2,6 +2,10 @@
 
 # g++4.8.1
 if [ "$CXX" == "g++" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; fi
+echo 'added ppa'
 sudo apt-get update -qq
+echo 'apt-get update'
 if [ "$CXX" == "g++" ]; then sudo apt-get install -qq g++-4.8; fi
+echo 'download g++-4.8'
 if [ "$CXX" == "g++" ]; then export CXX="g++-4.8"; fi
+echo 'set CXX variable'
