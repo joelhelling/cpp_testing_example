31668b56748b03757275acbc0635d0962723a49c
spencewenski <sferris37@gmail.com>
2015-08-13 13:13:33 -0400

    get 100% code coverage for Bad_coverage

--- Bad_coverage_test.cpp
+++ Bad_coverage_test.cpp
@@ -3,5 +3,9 @@
 
 TEST(Bad_coverage_test, blah_blah)
 {
-
+  EXPECT_EQ(2, blah_blah(1));
+  EXPECT_EQ(4, blah_blah(3));
+  EXPECT_EQ(6, blah_blah(5));
+  EXPECT_EQ(-7, blah_blah(7));
+  EXPECT_EQ(0, blah_blah(0));
 }
